fb9567302a530938812ae731a18a0df807efcd0a
Sourcery AI <>
2021-12-30 17:38:22 +0000

    'Refactored by Sourcery'

--- accounts/views.py
+++ accounts/views.py
@@ -32,108 +32,98 @@ class ValidatePhoneSendOTP(APIView):
         username = request.data.get('username', False)
         email = request.data.get('email', False)
 
-        if phone_number:
-            phone = str(phone_number)
-            user = User.objects.filter(phone__iexact=phone)
-            if user.exists():
-                return Response({
-                    'status': False,
-                    'detail': 'Phone number already exists'
-                })
-
-            else:
-                key = send_otp(phone)
-                if key:
-                    old = PhoneOTP.objects.filter(phone__iexact=phone)
-                    if old.exists():
-                        old = old.first()
-                        count = old.count
-                        if count > 10:
-                            return Response({
-                                'status': False,
-                                'detail': 'Sending otp error. Limit Exceeded. Please Contact Customer support'
-                            })
-
-                        old.count = count + 1
-                        old.save()
-                        print('Count Increase', count)
-
-                        conn.request("GET",
-                                     "https://2factor.in/API/R1/?module=SMS_OTP&apikey=f0c241f6-025e-11ec-a13b-0200cd936042=" + phone + "&otpvalue=" + str(
-                                         key) + "&templatename=Casper")
-                        res = conn.getresponse()
-
-                        data = res.read()
-                        data = data.decode("utf-8")
-                        data = ast.literal_eval(data)
-
-                        if data["Status"] == 'Success':
-                            old.otp_session_id = data["Details"]
-                            old.save()
-                            print('In validate phone :' + old.otp_session_id)
-                            return Response({
-                                'status': True,
-                                'detail': 'OTP sent successfully'
-                            })
-                        else:
-                            return Response({
-                                'status': False,
-                                'detail': 'OTP sending Failed'
-                            })
-
-
-
-
-                    else:
-
-                        obj = PhoneOTP.objects.create(
-                            phone=phone,
-                            otp=key,
-                            email=email,
-                            username=username,
-                            password=password,
-                        )
-                        conn.request("GET",
-                                     "https://2factor.in/API/R1/?module=SMS_OTP&apikey=f0c241f6-025e-11ec-a13b-0200cd936042=" + phone + "&otpvalue=" + str(
-                                         key) + "&templatename=Casper")
-                        res = conn.getresponse()
-                        data = res.read()
-                        print(data.decode("utf-8"))
-                        data = data.decode("utf-8")
-                        data = ast.literal_eval(data)
-
-                        if data["Status"] == 'Success':
-                            obj.otp_session_id = data["Details"]
-                            obj.save()
-                            print('In validate phone :' + obj.otp_session_id)
-                            return Response({
-                                'status': True,
-                                'detail': 'OTP sent successfully'
-                            })
-                        else:
-                            return Response({
-                                'status': False,
-                                'detail': 'OTP sending Failed'
-                            })
-
-
-                else:
-                    return Response({
-                        'status': False,
-                        'detail': 'Sending otp error'
-                    })
-
-        else:
+        if not phone_number:
             return Response({
                 'status': False,
                 'detail': 'Phone number is not given in post request'
             })
+        phone = str(phone_number)
+        user = User.objects.filter(phone__iexact=phone)
+        if user.exists():
+            return Response({
+                'status': False,
+                'detail': 'Phone number already exists'
+            })
+
+        key = send_otp(phone)
+        if not key:
+            return Response({
+                'status': False,
+                'detail': 'Sending otp error'
+            })
+
+        old = PhoneOTP.objects.filter(phone__iexact=phone)
+        if old.exists():
+            old = old.first()
+            count = old.count
+            if count > 10:
+                return Response({
+                    'status': False,
+                    'detail': 'Sending otp error. Limit Exceeded. Please Contact Customer support'
+                })
+
+            old.count = count + 1
+            old.save()
+            print('Count Increase', count)
+
+            conn.request("GET",
+                         "https://2factor.in/API/R1/?module=SMS_OTP&apikey=f0c241f6-025e-11ec-a13b-0200cd936042=" + phone + "&otpvalue=" + str(
+                             key) + "&templatename=Casper")
+            res = conn.getresponse()
+
+            data = res.read()
+            data = data.decode("utf-8")
+            data = ast.literal_eval(data)
+
+            if data["Status"] != 'Success':
+                return Response({
+                    'status': False,
+                    'detail': 'OTP sending Failed'
+                })
+
+
+
+
+            old.otp_session_id = data["Details"]
+            old.save()
+            print('In validate phone :' + old.otp_session_id)
+        else:
+
+            obj = PhoneOTP.objects.create(
+                phone=phone,
+                otp=key,
+                email=email,
+                username=username,
+                password=password,
+            )
+            conn.request("GET",
+                         "https://2factor.in/API/R1/?module=SMS_OTP&apikey=f0c241f6-025e-11ec-a13b-0200cd936042=" + phone + "&otpvalue=" + str(
+                             key) + "&templatename=Casper")
+            res = conn.getresponse()
+            data = res.read()
+            print(data.decode("utf-8"))
+            data = data.decode("utf-8")
+            data = ast.literal_eval(data)
+
+            if data["Status"] != 'Success':
+                return Response({
+                    'status': False,
+                    'detail': 'OTP sending Failed'
+                })
+
+
+            obj.otp_session_id = data["Details"]
+            obj.save()
+            print('In validate phone :' + obj.otp_session_id)
+        return Response({
+            'status': True,
+            'detail': 'OTP sent successfully'
+        })
 
 
 def send_otp(phone):
-    if phone:
-        key = random.randint(999, 9999)
-        print(key)
-        return key
-    else:
+    if not phone:
         return False
+    key = random.randint(999, 9999)
+    print(key)
+    return key
